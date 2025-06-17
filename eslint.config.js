d3e1f5496682760e4a61573c9a1d1f2fd6480983
Andriy Shloser <shloser.andriy@gmail.com>
2025-06-17 18:56:40 +0300

    added prettier to project

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import prettierConfig from './.prettierrc.json' assert { type: 'json' };
 
 export default defineConfig([
   {
@@ -11,5 +13,25 @@ export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
     languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+      },
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': ['warn', prettierConfig],
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      semi: ['warn', 'always'],
+    },
   },
 ]);
